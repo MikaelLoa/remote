c25d333e37a04b5b4117322c60fb464029a2decf
unknown <21N10012@student.unika.ac.id>
2023-12-22 10:08:37 +0700

    Menamabahkam file index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP Page</title>
+</head>
+<body>
+
+<?php
+    // PHP code goes here
+    $message = "Hello, World!";
+    echo "<h1>$message</h1>";
+?>
+
+</body>
+</html>
